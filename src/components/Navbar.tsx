e3dec256255f4b4cb5a7d7f7b7d2bef1b4bac7de
akbarirazia <akbariharis14@gmail.com>
2024-04-25 15:32:11 +0430

    navbar foxes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,90 @@
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <div>
-      <nav className="bg-green-600 p-4 border-b border-gray-300   ">
-        <div className="container flex justify-between mx-auto my-0">
-          <div className="flex items-center text-3xl font-extrabold text-white">
-            {/* <img src={logo} alt="Innovative Waris Logo" className="w-10 mr-2" /> */}
+    <nav className="border-gray-200 bg-green-600">
+      <div className="container flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white ">
             Innovative W
-          </div>
-          <ul className="list-none flex justify-end items-center">
-            <li className="mx-2">
-              <Link to="/" className=" hover:text-black text-white">
+          </span>
+        </Link>
+        <button
+          type="button"
+          onClick={toggleMenu}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2 text-white hover:bg-green-800 focus:ring-gray-600"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+            <li>
+              <Link
+                to="/"
+                className="block py-2 px-3  rounded md:bg-transparent  md:p-0 text-white  hover:text-black"
+                aria-current="page"
+              >
                 Home
               </Link>
             </li>
-            <li className="mx-2">
-              <Link to="/about" className=" hover:text-black text-white">
-                About
-              </Link>
-            </li>
-            <li className="mx-2">
-              <Link to="/services" className=" hover:text-black text-white">
+            <li>
+              <Link
+                to="/services"
+                className="block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white  hover:text-black"
+              >
                 Services
               </Link>
             </li>
-            <li className="mx-2">
-              <Link to="/contact" className=" hover:text-black text-white">
+            <li>
+              <Link
+                to="/about"
+                className="block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white  hover:text-black"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white  hover:text-black"
+              >
                 Contact
               </Link>
             </li>
+            {/* Add more menu items as needed */}
           </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   )
 }
 
